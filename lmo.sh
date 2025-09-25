48baf2e84fb0bff6ee64d7ce472e00814e1333e1
MyRora <rivaldydanar123@gmail.com>
2025-09-25 08:14:33 +0700

    Update and rename README.md to lmo.sh

---
+++ lmo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Remove Clangs
+rm -rf prebuilts/clang/host/linux-x86
+# Repo Init
+repo init -u https://github.com/RoraaaAisyy/lmodroid_manifest.git -b fourteen --git-lfs
+/opt/crave/resync.sh | repo sync
+# cloning trees
+git clone https://github.com/RoraaaAisyy/android_device_xiaomi_earth.git -b Lmodroid-14 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/signing_keys.git -b Lmodroid vendor/lmodroid-priv/keys 
+# Start Build !
+. build/envsetup.sh
+breakfast earth userdebug
+mka bacon -j$(nproc --all)
